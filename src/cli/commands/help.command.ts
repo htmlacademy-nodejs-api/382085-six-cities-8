e16b1adb54e2c67a9b5b859844a50d69b6cc3ba5
George G <grreulvi@gmail.com>
2024-10-14 02:07:33 +0300

    [module1-task2] раскрасил вывод

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements Command {
   public getName(): string {
@@ -7,14 +8,14 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-      Программа для подготовки данных для REST API сервера.
-      Пример:
-        cli.js --<command> [arguments]
-      Команды:
-          --version:                      # выводит номер версии
-          --help:                         # печатает этот текст
-          --import <path>:                # импортирует данные из TSV
-          --generate <n> <path> <url>:    # генерирует произвольное количество
+      ${chalk.underline('Программа для подготовки данных для REST API сервера.')}
+      ${chalk.bold('Пример:')}
+        ${chalk.cyan('cli.js')} ${chalk.bold.yellow('--<command>')} ${chalk.magentaBright('[arguments]')}
+      ${chalk.bold('Команды:')}
+          ${chalk.bold.yellow('--version')}:                      ${chalk.bgGreenBright.black('# выводит номер версии')}
+          ${chalk.bold.yellow('--help')}:                         ${chalk.bgGreenBright.black('# печатает этот текст')}
+          ${chalk.bold.yellow('--import')} ${chalk.magentaBright('<path>')}:                ${chalk.bgGreenBright.black('# импортирует данные из TSV')}
+          ${chalk.dim('--generate <n> <path> <url>:    # генерирует произвольное количество тестовых данных')}
     `);
   }
 }
