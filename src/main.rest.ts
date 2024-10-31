dee914fd6bc1e3366226acbf8e8dd9d095c376b4
George G <grreulvi@gmail.com>
2024-10-31 03:24:19 +0300

    [module3-task1] change application to support configuration

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,11 +1,13 @@
 import { RestApplication } from './rest/rest.application.js';
+import { RestConfig } from './shared/libs/config/rest-config.js';
 import { PinoLogger } from './shared/libs/logger/pino-logger.js';
 
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const application = new RestApplication(logger);
+  const application = new RestApplication(logger, config);
   await application.init();
 }
 
--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,12 +1,19 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config } from '../shared/libs/config/index.js';
+import { RestSchema } from '../shared/libs/config/rest.schema.js';
 
 export class RestApplication {
 
   constructor(
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
     this.logger.info('Rest application initialization');
+    this.logger.info(`port is ${this.config.get('PORT')}`);
+    this.logger.info(`port is ${this.config.get('SALT')}`);
+    this.logger.info(`port is ${this.config.get('DB_HOST')}`);
   }
 }
+
